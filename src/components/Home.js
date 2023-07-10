b3f521c2090f23e9287cf4f303162a908c803a76
alphaone2468 <129504534+alphaone2468@users.noreply.github.com>
2023-07-10 20:14:21 +0530

    update code

--- src/components/Home.js
+++ src/components/Home.js
@@ -36,8 +36,8 @@ export default function Home() {
         email:b
       }
       setloading(true);
-      //console.log(loading)
-      const f=await fetch("https://ryobackend.onrender.com/getpost",{
+      console.log(loading)
+      const f=await fetch("http://localhost:5000/getpost",{
         method:'POST',
         body:JSON.stringify(objn),
         headers:{
@@ -45,17 +45,17 @@ export default function Home() {
         }
       })
       const res=await f.json()
-      //console.log(res)
+      console.log(res)
       try{
-        //console.log(res[0].comments)
+        console.log(res[0].comments)
 
       }
       catch{
-        //console.log("error");
+        console.log("error");
       }
       res.map((e)=>{
         var arr=e.comments[0].ratings;
-        //console.log(e.comments[0].ratings);
+        console.log(e.comments[0].ratings);
         var sum=0;
         for (let index = 0; index < arr.length; index++) {
           sum=sum+arr[index]
@@ -63,29 +63,29 @@ export default function Home() {
         }
         var avg=sum/arr.length;
 
-        //console.log(avg);
+        console.log(avg);
         rate.push(avg);
       })
       setloading(false);
-      //console.log(res)
-      //console.log(rate)
+      console.log(res)
+      console.log(rate)
       setrat(rate)
       setdata(res)
     }
 
   function handlenewratings(val){
-    // //console.log(" i sm calling" , val)
+    // console.log(" i sm calling" , val)
     var r=localStorage.getItem("deleteme");
-    // //console.log(" i  am from home and value is ",r);
+    // console.log(" i  am from home and value is ",r);
     var c=document.getElementById(`count${val}`).innerHTML.slice(17,19)
-    // //console.log("i am c of val",c)
+    // console.log("i am c of val",c)
     var mul=document.getElementById(`rating${val}`).innerHTML.slice(16,40)
-    // //console.log(mul)
+    // console.log(mul)
     var finalvalue=eval(mul*c)
-    // //console.log(finalvalue)
+    // console.log(finalvalue)
     var af=finalvalue+parseInt(r);
     var seriousfinal=af/(parseInt(c)+1)
-    // //console.log(seriousfinal)
+    // console.log(seriousfinal)
     document.getElementById(`rating${val}`).innerHTML=`OverAll rating  : ${seriousfinal}`
     document.getElementById(`count${val}`).innerHTML=`No. of Ratings   : ${parseInt(c)+1}`
     document.getElementById(`urated${val}`).style.display='block';
@@ -96,11 +96,11 @@ export default function Home() {
     },2000)
   }
   function handlenext(){
-    // //console.log("i am next")
+    // console.log("i am next")
     nav("/back")
   }
   async function handlecomm(ids){
-    // //console.log(comm)
+    // console.log(comm)
     var n=localStorage.getItem('ryo')
     const obj={
       comment:comm,
@@ -108,8 +108,8 @@ export default function Home() {
       id:ids
     }
     setcommentloading(true);
-    // //console.log(obj)
-    const f = await fetch("https://ryobackend.onrender.com/comment",{
+    // console.log(obj)
+    const f = await fetch("http://localhost:5000/comment",{
       method:'PUT',
       body:JSON.stringify(obj),
       headers:{
@@ -117,7 +117,7 @@ export default function Home() {
       }
     })
     const res= await f.json();
-    // //console.log(res)
+    // console.log(res)
     setcommentloading(false);
     if(res.success == "comment posted"){
       document.getElementById(`upperdiv${ids}`).style.display='block';
@@ -128,12 +128,12 @@ export default function Home() {
     }
   }
   function handleMoreComments(val){
-    // //console.log(val)
+    // console.log(val)
     localStorage.setItem("ryocomm",val)
     nav("/morecomments")
   }
   async function handleshare(id){
-    //console.log("share ", id);
+    console.log("share ", id);
     await navigator.share({
       title:"Hey! How Much Would You Rate This Outfit On A Scale Of 10 ",
       text:`Hey! How Much Would You Rate This Outfit On A Scale Of 10\nTo See The Outfit\nClick On The Link Given Below.\nYou Will See A *Menu* In That Click On *SearchPost*\nThen Search This *Id*.\n*Id* = ${id}\n`,
@@ -146,7 +146,7 @@ export default function Home() {
     const obj={
       email:e
     }
-    const f=await fetch("https://ryobackend.onrender.com/reset",{
+    const f=await fetch("http://localhost:5000/reset",{
       method:"post",
       body:JSON.stringify(obj),
       headers:{
@@ -154,14 +154,16 @@ export default function Home() {
       }
     })
     await f.json();
-    // //console.log(res);
+    // console.log(res);
   }
   function handleopenclose(){
-    //console.log("calling")
+    console.log("calling")
     if(open){
+      document.getElementById("hideprofile").style.display="block";
       setopen(false)
     }
     else{
+      document.getElementById("hideprofile").style.display="none";
       setopen(true);
 
     }
@@ -178,27 +180,23 @@ export default function Home() {
     <div className="tophomecon">
 
     <div className="postcon">
-    <div className="floatr">
-    <span id="one"></span>
-    <img src={photo} alt="" id="profileimg" className="makeMePointer"/>
-    </div>
-    <div className="upline">
-    <div className='opennavbar' onClick={handleopenclose}>
-
-    </div>
-    </div>
-    <div style={{display:"flex",flexDirection:'row-reverse'}} onClick={handleopenclose}>
+    <div style={{display:"flex",flexDirection:'row-reverse'}} onClick={handleopenclose} className='addborder1'>
     <img src={lines} alt="error" className='postimg'/>
+    </div>
+    <p className='maketopsome'>RateYourOutfit</p>
+    <div className="floatr" id='hideprofile'>
+    <span id="one"></span>
+    <img src={photo} alt="" id="profileimg" className="makeMePointer" />
     </div>
     {(open) ? 
     <div className="navv" id="hidenavv">
       <Link to="/postimage" className='linkofnav'><p>Post A Image</p></Link>
       <Link to="/searchuser" className='linkofnav'> <p>Search</p></Link>
       <Link to="/yourpost" className='linkofnav'> <p>Your Posts</p></Link>
-      <Link to="/login" className='linkofnav'> <p>logout</p></Link>
       <Link to="/contactme" className='linkofnav'> <p>Contact Us</p></Link>
       <Link to="/searchpost" className='linkofnav'> <p>Search Post</p></Link>
       <Link to="/following" className='linkofnav'> <p>Following</p></Link>
+      <Link to="/login" className='linkofnav'> <p>logout</p></Link>
     </div> : <p></p>
 }
     <div className='makespacetop'></div>
